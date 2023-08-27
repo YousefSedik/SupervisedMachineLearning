3070d6e0a6aee25d7e1675e6d648e08a308388c2
yousefsedik <joemohmmed00@gmail.com>
2023-08-27 20:55:39 +0300

    Unvariate Rgression

--- Regression/Models/UnivariateRegression.py
+++ Regression/Models/UnivariateRegression.py
@@ -1,2 +1,101 @@
+import numpy as np 
+import math, copy 
+import matplotlib.pyplot as plt 
+plt.style.use('deeplearning.mplstyle')
+from lab_utils_uni import plt_house_x, plt_contour_wgrad, plt_divergence, plt_gradients
+
+# Load our data set
+x_train = np.array([1.0, 2.0])   #features
+y_train = np.array([300.0, 500.0])   #target value
+def fx(x,w,b):
+    return w*x + b 
+# Function to calculate the cost
+def compute_cost(x, y, w, b):
+    
+    m = x.shape[0] # Get The Number Of The Example 
+    cost = 0
+    # 1/(2m) * sum((f(x[i])^ - y)**2)
+    for i in range(m):
+        f_wb = w * x[i] + b
+        cost = cost + (f_wb - y[i])**2
+    final_cost = (1 / (2 * m) )* cost
+
+    return final_cost
+
+def compute_gradient(x,y,w,b):
+    # x predected value 
+    # y actual value 
+    m = x.shape[0]
+    ret_for_w = 0 
+    ret_for_b = 0 
+
+    for i in range(m):
+        f_x = fx(x[i],w,b)
+        # W 
+        ret_for_w += ((f_x - y[i])*x[i])
+        # B
+        ret_for_b += ((f_x - y[i]))
+        
+    ret_for_w = (1/m)*ret_for_w 
+    ret_for_b = (1/m)*ret_for_b 
+    
+    return ret_for_w, ret_for_b
+
+def gradient_descent(x, y, w_in, b_in, alpha, num_iters, cost_function, gradient_function): 
+    """
+    Performs gradient descent to fit w,b. Updates w,b by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      x (ndarray (m,))  : Data, m examples 
+      y (ndarray (m,))  : target values
+      w_in,b_in (scalar): initial values of model parameters  
+      alpha (float):     Learning rate
+      num_iters (int):   number of iterations to run gradient descent
+      cost_function:     function to call to produce cost
+      gradient_function: function to call to produce gradient
+      
+    Returns:
+      w (scalar): Updated value of parameter after running gradient descent
+      b (scalar): Updated value of parameter after running gradient descent
+      J_history (List): History of cost values
+      p_history (list): History of parameters [w,b] 
+      """
+    
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    p_history = []
+    b = b_in
+    w = w_in
+    
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters using gradient_function
+        dj_dw, dj_db = gradient_function(x, y, w , b)     
+
+        # Update Parameters using equation (3) above
+        b = b - alpha * dj_db                            
+        w = w - alpha * dj_dw                            
+
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append( cost_function(x, y, w , b))
+            p_history.append([w,b])
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0:
+            print(f"Iteration {i:4}: Cost {J_history[-1]:0.2e} ",
+                    f"dj_dw: {dj_dw: 0.3e}, dj_db: {dj_db: 0.3e}  ",
+                    f"w: {w}, b:{b}")
+
+    return w, b, J_history, p_history #return w and J,w history for graphing
 
 
+# initialize parameters
+w_init = 0
+b_init = 0
+# some gradient descent settings
+iterations = 10000
+tmp_alpha = 1.0e-2
+# run gradient descent
+w_final, b_final, J_hist, p_hist = gradient_descent(x_train ,y_train, w_init, b_init, tmp_alpha, iterations, compute_cost, compute_gradient)
+
+print(f"(w,b) found by gradient descent: ({w_final},{b_final})")
